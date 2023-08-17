f5f810a94d50d0b86a14889188ba700655e8fd8a
Alice Souza <alicefms@hotmail.com>
2023-08-17 15:37:56 -0300

    alteração: traz o beforeEach para dentro da suit

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,11 +1,9 @@
 /// <reference types="Cypress" />
-beforeEach(() => {
-    cy.visit('./src/index.html')
-});
-
-
 describe('Central de Atendimento ao Cliente TAT', function() {
-    
+    beforeEach(() => {
+        cy.visit('./src/index.html')
+    });
+
     it('verifica o título da aplicação', function() {
         cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
     });
