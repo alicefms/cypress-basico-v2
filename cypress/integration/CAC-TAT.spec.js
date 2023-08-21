b6cf6c841a2f044e9afca6e3fd07a864f44f54ea
Alice Souza <alicefms@hotmail.com>
2023-08-21 10:24:53 -0300

    uso do click e do tick

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -8,14 +8,17 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
     });
     
-    it('preenche os campos obrigatórios e envia o formulário', () => {
+    it.only('preenche os campos obrigatórios e envia o formulário', () => {
         cy.get('#firstName').type('Alice')
         cy.get('#lastName').type('Souza')
         cy.get('#email').type('alice@email.com')
         cy.get('#phone').type('8199999999')
         cy.get('#open-text-area').type('eita, eita, vamos ver se agora demora menos pra digitar por causa do delay', {"delay": 0})
+        cy.clock()
         cy.contains('.button', 'Enviar').click()
         cy.get('.success').should('be.visible')
+        cy.tick(3000)
+        cy.get('.success').should('not.be.visible')
     });
 
     it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
