7f80549177e50f349ba7ad2ec760e237a507b241
Alice Souza <alicefms@hotmail.com>
2023-08-21 11:01:31 -0300

    uso dos comandos hide e show

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -118,6 +118,15 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('a').invoke('removeAttr', 'target').click()
         cy.get('#title').should('have.text', 'CAC TAT - Política de privacidade')
     });
+
+    it.only('exibe e esconde as mensagens de sucesso e erro usando o .invoke()', () => {
+        cy.get('.success').should('not.be.visible')
+        .invoke('show').should('be.visible').and('contain', 'Mensagem enviada com sucesso.')
+        .invoke('hide').should('not.be.visible')
+        cy.get('.error').should('not.be.visible')
+        .invoke('show').should('be.visible').and('contain', 'Valide os campos obrigatórios!')
+        .invoke('hide').should('not.be.visible')
+    });
 });
 
 
