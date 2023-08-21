3ae40cab6f1844fff6357b27a0d4a5b857c0e96a
Alice Souza <alicefms@hotmail.com>
2023-08-21 11:12:56 -0300

    uso do invoke 'val' para escrever strings longas mais rápido

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -119,13 +119,18 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#title').should('have.text', 'CAC TAT - Política de privacidade')
     });
 
-    it.only('exibe e esconde as mensagens de sucesso e erro usando o .invoke()', () => {
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke()', () => {
         cy.get('.success').should('not.be.visible')
         .invoke('show').should('be.visible').and('contain', 'Mensagem enviada com sucesso.')
         .invoke('hide').should('not.be.visible')
         cy.get('.error').should('not.be.visible')
         .invoke('show').should('be.visible').and('contain', 'Valide os campos obrigatórios!')
         .invoke('hide').should('not.be.visible')
+    });
+
+    it.only('preenche a area de texto usando o comando invoke', () => {
+        const conteudo = Cypress._.repeat("texto a ser repetido \n", 5)
+        cy.get('#open-text-area').invoke('val',conteudo).should('have.value', conteudo)
     });
 });
 
